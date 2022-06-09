c7d40e615c7615ad50184cf6bd29f6d24a1f4c53
Flobsterable <loki55rus@gmail.com>
2022-06-09 17:25:31 +0600

    added images for login and register screen

--- app/src/main/java/com/example/pets_project/ui/screens/login/LoginScreen.kt
+++ app/src/main/java/com/example/pets_project/ui/screens/login/LoginScreen.kt
@@ -1,28 +1,35 @@
 package com.example.pets_project.ui.screens.login
 
+import android.content.res.Resources
 import android.widget.EditText
+import android.widget.Switch
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.R
+
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.pets_project.ui.theme.PetsprojectTheme
+import com.example.pets_project.R
+
+
 
 @Composable
 fun LoginScreen(){
@@ -39,14 +46,24 @@ fun LoginScreen(){
     }
 
 }
-
+@Preview(showBackground = true)
 @Composable
 fun SwitchLoginRow(){
     Row(modifier = Modifier
         .fillMaxWidth()
         .padding(start = 32.dp, end = 32.dp)) {
-        Text(text = "Вход")
-        Text(text = "Регстрация")
+        //SwitchLoginButton(idName = R.string.switch_login)
+        SwitchLoginButton(idName = R.string.switch_registration)
+    }
+}
+
+@Composable
+fun SwitchLoginButton(idName: Int){
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Text(textAlign = TextAlign.Center,
+            modifier = Modifier.padding(start = 12.dp, bottom = 7.dp),
+            text = stringResource(id = idName))
+        Divider(color = Color.Blue, thickness = 5.dp)
     }
 }
 
@@ -61,7 +78,7 @@ fun LoginBox(){
                 .padding(top = 40.dp, start = 32.dp, end = 32.dp),
             value = value,
             onValueChange = {value = it},
-            placeholder = {Text("email")},
+            placeholder = {Text(text = stringResource(id = R.string.edit_text_email))},
             maxLines = 1,
         )
 
@@ -71,24 +88,23 @@ fun LoginBox(){
                 .padding(top = 32.dp, start = 32.dp, end = 32.dp),
             value = value,
             onValueChange = {value = it},
-            placeholder = {Text("pass")},
+            placeholder = {Text(text = stringResource(id = R.string.edit_text_password))},
             maxLines = 1,
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
         )
 
         Button(onClick = { /*TODO*/ },
             modifier = Modifier
-                .height(56.dp)
                 .fillMaxWidth()
                 .padding(top = 40.dp, start = 114.dp, end = 114.dp)
         ) {
-            Text(stringResource(id = R.string.login_button)
-
+            Image(painter = painterResource(id = R.drawable.ic_mark), contentDescription ="Mark Login Button" )
+            Text(text = stringResource(id = R.string.button_login))
         }
 
         Text(
             modifier = Modifier.padding(top = 52.dp, start = 122.dp, end = 122.dp),
-            text = "забыли пароль",
+            text = stringResource(id = R.string.text_button_fogot),
             style = TextStyle(
                 color = Color.Black,
                 fontSize = 16.sp)
@@ -105,10 +121,11 @@ fun RegistrationBox(){
         TextField(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(top = 40.dp, start = 32.dp, end = 32.dp),
+                .padding(top = 40.dp, start = 32.dp, end = 32.dp)
+                .shadow(10.dp),
             value = value,
             onValueChange = {value = it},
-            placeholder = {Text("Имя")},
+            placeholder = {Text(stringResource(id = R.string.edit_text_name))},
             maxLines = 1,
         )
         TextField(
@@ -117,7 +134,7 @@ fun RegistrationBox(){
                 .padding(top = 32.dp, start = 32.dp, end = 32.dp),
             value = value,
             onValueChange = {value = it},
-            placeholder = {Text("email")},
+            placeholder = {Text(stringResource(id = R.string.edit_text_email))},
             maxLines = 1,
         )
 
@@ -127,7 +144,7 @@ fun RegistrationBox(){
                 .padding(top = 32.dp, start = 32.dp, end = 32.dp),
             value = value,
             onValueChange = {value = it},
-            placeholder = {Text("pass")},
+            placeholder = {Text(stringResource(id = R.string.edit_text_password))},
             maxLines = 1,
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
         )
@@ -137,7 +154,7 @@ fun RegistrationBox(){
                 .padding(top = 32.dp, start = 32.dp, end = 32.dp),
             value = value,
             onValueChange = {value = it},
-            placeholder = {Text("pass")},
+            placeholder = {Text(stringResource(id = R.string.edit_text_repeat_password))},
             maxLines = 1,
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
         )
@@ -147,8 +164,11 @@ fun RegistrationBox(){
                 .fillMaxWidth()
                 .padding(top = 40.dp, start = 56.dp, end = 56.dp)
         ) {
-            Text("Зарегистрироваться")
-
+            Row(modifier = Modifier.padding(start = 32.dp,end = 32.dp)) {
+                Image(painter = painterResource(id = R.drawable.ic_mark), contentDescription ="Registration Button Mark" )
+                Spacer(modifier = Modifier.padding(start = 10.dp))
+                Text(stringResource(id = R.string.button_registration))
+            }
         }
 
     }
@@ -156,25 +176,33 @@ fun RegistrationBox(){
 
 @Composable
 fun ImageHeader(){
+
     Box(modifier = Modifier
-        .background(color = Color.Gray)
         .fillMaxWidth()
-        .height(140.dp) )
+        .height(140.dp) ){
+        Image(painter = painterResource(id = R.drawable.logo_header), contentDescription ="Logo Header",
+        modifier = Modifier.padding(top = 56.dp,start = 104.dp, end = 104.dp))
+    }
 }
 
 @Composable
 fun NoLoginButton(){
     Row(modifier = Modifier
         .fillMaxWidth()
-        .padding(start = 92.dp, end = 92.dp)) {
+        .padding(start = 92.dp, end = 92.dp, bottom = 64.dp)) {
         Text(
-            modifier = Modifier.padding(bottom = 84.dp),
-            text = "войти позже",
+            modifier = Modifier.padding(start = 32.dp,top = 20.dp,bottom = 20.dp),
+            text = stringResource(id = R.string.text_button_no_login),
             style = TextStyle(
-                color = Color.Black,
+                color = MaterialTheme.colors.onSecondary,
                 fontSize = 16.sp,
+                fontWeight = FontWeight(600)
             )
         )
+        Spacer(modifier = Modifier.padding(start = 8.dp))
+        Image(painter = painterResource(id = R.drawable.ic_arrow_button), contentDescription = "",
+        modifier = Modifier.padding(top = 22.dp, bottom = 20.dp))
+       
 
     }
 }
--- app/src/main/java/com/example/pets_project/ui/theme/Type.kt
+++ app/src/main/java/com/example/pets_project/ui/theme/Type.kt
@@ -1,18 +1,50 @@
 package com.example.pets_project.ui.theme
 
 import androidx.compose.material.Typography
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.pets_project.R
+import com.example.pets_project.ui.theme.Typography
 
 // Set of Material typography styles to start with
+private val mulish = FontFamily(
+    Font(R.font.poppins_black, weight = FontWeight.Black),
+    Font(R.font.poppins_black_italic,weight = FontWeight.Black, style = FontStyle.Italic),
+    Font(R.font.poppins_bold, weight = FontWeight.Bold),
+    Font(R.font.poppins_bold_italic,weight = FontWeight.Bold, style = FontStyle.Italic),
+    Font(R.font.poppins_extra_bold, weight = FontWeight.ExtraBold),
+    Font(R.font.poppins_extra_bold_italic, weight = FontWeight.ExtraBold, style = FontStyle.Italic),
+    Font(R.font.poppins_extra_light, weight = FontWeight.ExtraLight),
+    Font(R.font.poppins_extra_light_italic,weight = FontWeight.ExtraLight, style = FontStyle.Italic),
+    Font(R.font.poppins_italic, style = FontStyle.Italic),
+    Font(R.font.poppins_light, weight = FontWeight.Light),
+    Font(R.font.poppins_light_italic, weight = FontWeight.Light, style = FontStyle.Italic),
+    Font(R.font.poppins_medium, weight = FontWeight.Medium),
+    Font(R.font.poppins_medium_italic,weight = FontWeight.Medium, style = FontStyle.Italic),
+    Font(R.font.poppins_regular),
+    Font(R.font.poppins_semi_bold, weight = FontWeight.SemiBold),
+    Font(R.font.poppins_semi_bold_italic, weight = FontWeight.SemiBold,style = FontStyle.Italic),
+    Font(R.font.poppins_thin, weight = FontWeight.Thin),
+    Font(R.font.poppins_thin_italic, weight = FontWeight.Thin ,style = FontStyle.Italic)
+)
+
 val Typography = Typography(
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
-    )
+    ),
+    button = TextStyle(
+        fontFamily = mulish,
+        fontWeight = FontWeight(700),
+        fontSize = 16.sp
+    ),
+
     /* Other default text styles to override
     button = TextStyle(
         fontFamily = FontFamily.Default,
@@ -26,3 +58,4 @@ val Typography = Typography(
     )
     */
 )
+
